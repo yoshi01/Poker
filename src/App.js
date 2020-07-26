c32dca8d8752b34b5197558909b17efec7259f69
yoshi01 <27726838+yoshi01@users.noreply.github.com>
2020-07-26 19:30:00 +0900

    feat: tmp

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Board from './components/Board.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">test</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Board>
+      </Board>
     </div>
   );
 }
